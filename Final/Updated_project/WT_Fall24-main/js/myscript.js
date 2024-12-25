2d7dcd88c99918e64e92bfd09a9600a0fa3a95fc
Tanvir Hossain <109126803+TRxTanvir@users.noreply.github.com>
2024-12-25 15:55:21 +0600

    Update myscript.js

--- Final/Updated_project/WT_Fall24-main/js/myscript.js
+++ Final/Updated_project/WT_Fall24-main/js/myscript.js
@@ -1,1 +1,38 @@
-t
+function formValidation(){
+if(checkQuantity())
+{
+    return true;
+}
+else{
+    return false;
+}
+}
+
+function checkQuantity(){
+
+    var quatity = document.getElementById("quantity").value;
+    if(quatity == ""){
+        document.getElementById("quantityError").innerHTML = "Must be a number";
+        return false;
+    }
+    else {
+        return true;
+    }
+
+}
+
+function searchOrder(){
+
+var order = document.getElementById("search").value;
+console.log(order);
+var xttp= new XMLHttpRequest();
+
+xttp.onreadystatechange = function () {
+if(this.readyState==4 && this.status==200)
+    document.getElementById("show").innerHTML = this.responseText;
+};
+
+
+xttp.open("GET","../control/ajaxrequest.php?search="+order,true);
+xttp.send();
+}
