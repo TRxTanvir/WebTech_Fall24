465ca15ea745096d9e9730291351e6e1845a15cf
Tanvir Hossain <109126803+TRxTanvir@users.noreply.github.com>
2024-12-25 16:01:00 +0600

    Add files via upload

---
+++ Final/Updated_project/WT_Fall24-main/layout/header.php
@@ -0,0 +1,21 @@
+<html>
+
+<body>
+<img src="../images/BANNER-HEADING.png">
+
+<div class="sticky">
+    <table>
+        <tr>
+            <td><a href="../view/Home.php">Home</a></td>
+            <td><a href="../view/userlogin.php">Login</a></td>
+            <td><a href="../view/userreg.php">Sign Up</a></td>
+            <td><a href="../view/creatreorder.php">Create Order</a></td>
+            <td><a href="../view/showorders.php">Show Order</td>
+            <td><a href="../view/showorderswithajax.php">Show Order Ajax</td>
+        </tr>
+</table>
+</div>
+
+
+</body>
+</html>
